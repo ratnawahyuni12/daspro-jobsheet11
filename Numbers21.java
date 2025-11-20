1fd467e1816d3815ac126982d9a3ac2d008dbbd0
ratnawahyuni12 <ratna120307@gmail.com>
2025-11-20 13:59:18 +0700

    Modifikasi percobaan 3 (perintah nomor 4)

--- Numbers21.java
+++ Numbers21.java
@@ -11,5 +11,9 @@ public class Numbers21 {
         for (int i = 0; i < myNumbers.length; i++) {
             System.out.println(Arrays.toString(myNumbers[i]));
         }
+
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println("Panjang baris ke-" + (i+1) + ": " + myNumbers[i].length);
+        }
     }
 }
