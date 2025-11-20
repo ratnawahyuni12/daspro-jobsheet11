fb08a7631155a08d7a21e8a364d6e52fc3423b8f
ratnawahyuni12 <ratna120307@gmail.com>
2025-11-20 13:32:52 +0700

    Modifikasi percobaan 2 (perintah nomor 5)

--- BioskopWithScanner21.java
+++ BioskopWithScanner21.java
@@ -67,7 +67,7 @@ public class BioskopWithScanner21 {
                     System.out.println("===== DAFTAR PENONTON =====");
                     for (int i = 0; i < penonton.length; i++) {
                         for (int j = 0; j < penonton[i].length; j++) {
-                            String isi = (penonton[i][j] == null) ? "---" : penonton[i][j];
+                            String isi = (penonton[i][j] == null) ? "***" : penonton[i][j];
                             System.out.println("Baris " + (i+1) + " Kolom " + (j+1) + ": " + isi);
                         }
                     }
